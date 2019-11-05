0139da143444064a672f71994f7c7e6265e174e1
johndahrah <dahrah@yandex.ru>
2019-11-05 20:50:14 +0300

    Initial Commit

---
+++ sendIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sendIT
+//
+//  Created by Иван Дахненко on 05.11.2019.
+//  Copyright © 2019 Ivan Dakhnenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
